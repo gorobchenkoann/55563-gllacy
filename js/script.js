eec3cdc2974ed8d57fbc4b368f5bbcb47755e921
gorobchenkoann <gorobchenko.ann@gmail.com>
2017-09-05 00:42:48 +0300

    добавлена валидация формы

--- js/script.js
+++ js/script.js
@@ -1,15 +1,50 @@
 var btn = document.querySelector('.contacts-btn');
 var modal = document.querySelector('.modal-feedback');
+var form = document.querySelector('.feedback-form');
 var close = document.querySelector('.close-btn');
-var map = document.querySelector('.map-img');
+var formname = modal.querySelector("[name=feedback-name]");
+var email = modal.querySelector("[name=feedback-email]");
+var comment = modal.querySelector("[name=feedback-comment]");
+var storage = localStorage.getItem("formname");
 
 btn.addEventListener('click', function(e) {
 	e.preventDefault();
 	modal.style.display = 'flex';
+	if (storage) {
+		formname.value = storage;
+		email.focus();
+	}
+	else {
+		formname.focus();
+	}
 });
 
 close.addEventListener('click', function(e) {
 	e.preventDefault();
 	modal.style.display = 'none';
+	modal.classList.remove('modal-error');
 });
+
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+	if (!formname.value || email.value || comment.value) {
+		e.preventDefault();
+		modal.classList.remove('modal-error');
+		modal.offsetWidth = modal.offsetWidth;
+		modal.classList.add('modal-error');		
+	}
+	else {
+		localStorage.setItem("formname", formname.value);
+	}
+});
+
+window.addEventListener("keydown", function (e) {
+    if (e.keyCode === 27) {
+		if (modal.style.display == 'flex') {
+		modal.style.display = 'none';
+		modal.classList.remove('modal-error');
+		}
+    }
+  });
+
 
